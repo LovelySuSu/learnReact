8db1cca75a0034958c846ccc8ab6f8aa4b2c18f8
dingding <dingding@dingdingdeMac-mini.local>
2018-08-03 18:18:46 +0800

    init proj

---
+++ src/TodoList.js
@@ -0,0 +1,34 @@
+import React, { Component } from 'react';
+
+import {
+    Header,
+    Logo,
+    Title,
+    ButtonList,
+    Button,
+    List
+} from './style'
+
+
+class TodoList extends Component {
+  render() {
+    return (
+      <div>
+        <Header>
+            <Logo></Logo>
+            <Title>丁丁的计划表</Title>
+            <ButtonList>
+                <Button className="current">全部</Button>
+                <Button>已完成</Button>
+                <Button>未完成</Button>
+            </ButtonList>
+        </Header>
+          <List>
+
+          </List>
+      </div>
+    );
+  }
+}
+
+export default TodoList;
